39c90ecdeb74a5f3070a4ceb365ee3a492597d89
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-11-12 21:58:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -37,7 +37,7 @@ end
 
 def take_a_number(array, name)
   array << name
-puts "Welcome, #{name}. You are number #{katz_deli[count] in line."
+puts "Welcome, #{name}. You are number #{katz_deli[count]} in line."
   #binding.pry
 end
 
