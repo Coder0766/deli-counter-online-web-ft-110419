ffc9432ae4b71f66198b66e586d9110d75a55ac3
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-11-12 22:32:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,8 @@ require 'pry'
 
 def line(katz_deli)
   
-  if katz_deli.count == 0 
+  if
+    katz_deli.count == 0 
     puts "The line is currently empty."
     else  
     count = 0
@@ -42,9 +43,11 @@ puts "Welcome, #{name}. You are number #{array.count} in line."
 end
 
 def now_serving(katz_deli)
-  if katz_deli.length == 0
+  if 
+  katz_deli.length == 0
   puts "There is nobody waiting to be served!"
-  else katz_deli.shift 
+  else 
+  katz_deli.shift 
   puts "Currently serving #{katz_deli.first}."
   end
 end
