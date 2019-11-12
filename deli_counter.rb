c0eec6a7f2f69b53fb9f3925bde557025a5d5075
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-11-12 21:09:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,15 +5,6 @@
 
 # first Thoughts => incrementing count 
 
-def line(array)
-  
-  katz_deli[]
-  array = katz_deli
-  index = i
-  array.length {|i| if i == 0 then puts "The line is currently empty" elsif i >= 1 then puts
-  "There are people in line" else puts "Now serving #{i}"}} end
-end
- 
 # def take_a_number(array, name)
 # end
 
@@ -38,9 +29,9 @@ def line(katz_deli)
     while count < katz_deli.count do  
     position_in_line[] << "#{count + 1}. #{katz_deli[count]}"
     count += 1
-        end
-     position_in_line "The line is currently empty." 
     end
+     position_in_line "The line is currently empty." 
+  end
 end 
 
 
