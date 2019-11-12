74a1027eda865dbaca7d4b5c6a413b437f966cf3
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-11-12 22:33:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -47,8 +47,8 @@ def now_serving(katz_deli)
   katz_deli.length == 0
   puts "There is nobody waiting to be served!"
   else 
-  katz_deli.shift 
   puts "Currently serving #{katz_deli.first}."
+  katz_deli.shift 
   end
 end
 
